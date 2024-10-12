c615f07580fba07af63519c4aa7582913ede5ee0
Sowmya Koneti <sowmyaa.koneti@gmail.com>
2024-10-11 21:45:10 -0500

    Updates

---
+++ precache-manifest.69f66512751176feffe5fb3733a11171.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce07022328f04aa4354a2120b3b652f",
+    "url": "/Serverless-security/index.html"
+  },
+  {
+    "revision": "8c3872ac0a868a9e9dae",
+    "url": "/Serverless-security/static/css/main.119d9826.chunk.css"
+  },
+  {
+    "revision": "f18e9280047d2c2f67b5",
+    "url": "/Serverless-security/static/js/2.afef58d2.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/Serverless-security/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "8c3872ac0a868a9e9dae",
+    "url": "/Serverless-security/static/js/main.291040cf.chunk.js"
+  },
+  {
+    "revision": "65b6dc32c7417febf11e",
+    "url": "/Serverless-security/static/js/runtime~main.e26d4383.js"
+  },
+  {
+    "revision": "3d775fa7983f91bd4e2f20f4c28ddc37",
+    "url": "/Serverless-security/static/media/Pravarsha.3d775fa7.png"
+  },
+  {
+    "revision": "9f602f6e5ae8b0e15d29a8066c271b45",
+    "url": "/Serverless-security/static/media/SaiKumar.9f602f6e.png"
+  },
+  {
+    "revision": "2475e907c6cdf5a5362132ebb7340340",
+    "url": "/Serverless-security/static/media/Sowjanya.2475e907.png"
+  },
+  {
+    "revision": "747f79b32f46cbdf11f083af9ea7b15d",
+    "url": "/Serverless-security/static/media/Sowmya.747f79b3.png"
+  },
+  {
+    "revision": "f18d7b1e2c8ffc01d50c54b9cb90152e",
+    "url": "/Serverless-security/static/media/vivek.f18d7b1e.png"
+  }
+]);
